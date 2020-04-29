fbe08c1fd2bb480961c0246b344d2ab361c17254
KolchMax <kolchurin10071998@mail.ru>
2020-04-30 02:38:23 +0500

    Write authentication, write current user on document

--- fsociety/urls.py
+++ fsociety/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.chat_view, name='chat'),
     url(r'^(?P<pk>[0-9]+)/get_messages$', views.get_messages, name='get_messages'),
-    url(r'^(?P<pk>[0-9]+)/set_messages$', views.set_messages, name='set_messages')
+    url(r'^(?P<pk>[0-9]+)/set_messages$', views.set_messages, name='set_messages'),
+    url(r'^logout$', views.logout_view, name="logout"),
+    url(r'^login$', views.login_view, name='login'),
+    url(r'^signin$', views.sign_in_form, name='sign_in'),
+    url(r'^about$', views.AboutView.as_view(), name='about')
 ]
--- fsociety/views.py
+++ fsociety/views.py
@@ -6,7 +6,8 @@ from django.contrib.auth import authenticate
 from .models import Message
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
+
 import json
 # Create your views here.
 
@@ -16,17 +17,30 @@ class IndexView(generic.ListView):
     context_object_name = 'user_list'
 
     def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return None
         print("index")
         return User.objects.order_by('-id')
 
 
+class AboutView(generic.ListView):
+    template_name = 'fsociety/about.html'
+
+    def get_queryset(self):
+        return None
+
+
 def chat_view(request, pk):
+    if not request.user.is_authenticated:
+        return render(request, 'fsociety/signin.html')
     print("chat_view")
     receiver = get_object_or_404(User, pk=pk)
     return render(request, 'fsociety/chat.html', {'receiver': receiver})
 
 
 def get_messages(request, pk):
+    if not request.user.is_authenticated:
+        return render(request, 'fsociety/signin.html')
     print("get_messages")
     msg1 = Message.objects.filter(receiver_id=pk).filter(sender_id=request.user.id)
     msg2 = Message.objects.filter(receiver_id=request.user.id).filter(sender_id=pk)
@@ -44,6 +58,8 @@ def get_messages(request, pk):
 
 
 def set_messages(request, pk):
+    if not request.user.is_authenticated:
+        return render(request, 'fsociety/signin.html')
     print("set_messages")
     print(request.user)
     receiver = get_object_or_404(User, pk=pk)
@@ -54,3 +70,30 @@ def set_messages(request, pk):
     print(new_msg)
     Message.save(new_msg)
     return render(request, 'fsociety/chat.html', {'receiver': receiver})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'fsociety/signin.html')
+
+
+def sign_in_form(request):
+    return render(request, 'fsociety/signin.html')
+
+
+def login_view(request):
+    print(request.POST)
+    user = authenticate(username=request.POST['username'],
+                        password=request.POST['password'])
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            print("User is valid, active and authenticated")
+            return HttpResponseRedirect(reverse('fsociety:index', args=()))
+        else:
+            print("The password is valid, but the account has been disabled!")
+            return render(request, 'fsociety/signin.html', {'message': 'Your account disabled'})
+    else:
+        # the authentication system was unable to verify the username and password
+        print("The username and password were incorrect.")
+        return render(request, 'fsociety/signin.html', {'message': 'Incorrect username or password'})
